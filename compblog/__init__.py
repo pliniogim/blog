e198ff86c269014d37b07c1e721fc9fe584ffad5
pliniogim <pliniogimenez@gmail.com>
2026-05-18 20:25:14 -0300

    blogposts

--- compblog/__init__.py
+++ compblog/__init__.py
@@ -36,10 +36,13 @@ login_manager.login_view = 'users.login'
 
 from compblog.core.views import core
 from compblog.users.views import users
+from compblog.posts.views import blog_posts
 from compblog.error_pages.handlers import error_pages
 
 app.register_blueprint(core)
 app.register_blueprint(users)
+app.register_blueprint(blog_posts)
 app.register_blueprint(error_pages)
+
 
 ###########################################
---
+++ compblog/posts/views.py
@@ -0,0 +1,72 @@
+from flask import abort, render_template, flash, redirect, request, url_for, Blueprint
+from flask_login import current_user, login_required
+from compblog import db
+from compblog.posts.forms import PostForm
+from compblog.models import BlogPost  
+
+blog_posts = Blueprint('blog_posts', __name__)
+
+# CREATE
+@blog_posts.route('/create', methods=['GET', 'POST'])
+@login_required
+def create_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = BlogPost(title=form.title.data, 
+                        text=form.text.data, 
+                        user_id=current_user.id)
+        db.session.add(post)
+        db.session.commit()
+        flash('Post Created!')
+        return redirect(url_for('core.index'))
+    return render_template('create_post.html', form=form)
+
+# Blog Post VIEW
+@blog_posts.route('/<int:blog_post_id>')
+def blog_post(blog_post_id):
+    blog_post = BlogPost.query.get_or_404(blog_post_id)
+    return render_template('blog_post.html', 
+                           title=blog_post.title, 
+                           date=blog_post.date, 
+                           post=blog_post)
+    
+# UPDATE
+@blog_posts.route('/<int:blog_post_id>/update', methods=['GET', 'POST'])
+@login_required
+def update(blog_post_id):
+    
+    blog_post = BlogPost.query.get_or_404(blog_post_id)
+    if blog_post.author != current_user:
+        abort(403)
+    form = PostForm()
+    
+    if form.validate_on_submit():
+        blog_post.title = form.title.data
+        blog_post.text = form.text.data
+        
+        db.session.commit()
+        flash('Post Updated!')
+        return redirect(url_for('blog_posts.blog_post', 
+                                blog_post_id=blog_post.id))
+        
+    elif request.method == 'GET':
+        form.title.data = blog_post.title
+        form.text.data = blog_post.text
+        
+    return render_template('create_post.html', title='Updating', form=form) 
+
+# DELETE
+@blog_posts.route('/<int:blog_post_id>/delete', methods=['POST'])
+@login_required
+def delete_post(blog_post_id):      
+    blog_post = BlogPost.query.get_or_404(blog_post_id)
+    if blog_post.author != current_user:
+        abort(403)
+    db.session.delete(blog_post)
+    db.session.commit()
+    flash('Post Deleted!')
+    return redirect(url_for('core.index'))
+
+
+
+  
