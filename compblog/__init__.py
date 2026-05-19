c3b35efcbb7652de444e8b2825e1df4d0daac9d7
pliniogim <pliniogimenez@gmail.com>
2026-05-19 20:24:20 -0300

    final_not_working

--- compblog/__init__.py
+++ compblog/__init__.py
@@ -2,12 +2,13 @@
 
 import os
 from flask import Flask
-from compblog.core.views import core
-from compblog.error_pages.handlers import error_pages
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-
+from compblog.core.views import core
+from compblog.users.views import users
+from compblog.posts.views import blog_posts
+from compblog.error_pages.handlers import error_pages
 
 
 app = Flask(__name__)
@@ -33,11 +34,6 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'users.login'
 
-
-from compblog.core.views import core
-from compblog.users.views import users
-from compblog.posts.views import blog_posts
-from compblog.error_pages.handlers import error_pages
 
 app.register_blueprint(core)
 app.register_blueprint(users)
--- compblog/core/views.py
+++ compblog/core/views.py
@@ -1,14 +1,16 @@
 # core/views.py
 
 from flask import render_template, request, Blueprint
+from compblog.models import BlogPost
 
 core = Blueprint('core', __name__)
 
 
 @core.route('/')
 def index():
-    # TODO
-    return render_template("index.html")
+    page = request.args.get('page', 1, type=int)
+    blog_posts = BlogPost.query.order_by(BlogPost.date.desc()).paginate(page=page, per_page=5)
+    return render_template("index.html", blog_posts=blog_posts)
 
 
 @core.route('/info')
